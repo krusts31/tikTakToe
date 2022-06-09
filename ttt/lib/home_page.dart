4d974a6855d0f2184246d32422d742bded27bde5
Andrew Nijssen <anijssen@f1r1s5.codam.nl>
2022-06-09 21:56:14 +0200

    homepage copied from tutorial at 6.14

---
+++ ttt/lib/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart'
+
+class HomePage extends StatefulWidget {
+	@override
+	_HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+	@override
+	Widget build(BuildContext context) {
+		return new Container();
+	}
+}
