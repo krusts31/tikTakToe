a6e97f75e8cbe528498907e8d4dcd66d218acd9b
krusts31 <alkrusts@student.codam.nl>
2022-06-09 23:06:19 +0200

    next step

--- ttt/lib/home_page.dart
+++ ttt/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ttt/game_button.dart';
 
 class HomePage extends StatefulWidget {
 	@override
@@ -6,8 +7,57 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+	List<GameButton> buttonsList = [new GameButton(id: 1)];
+
+		@override
+		void initState() {
+			super.initState();
+			buttonsList = doInit();
+		}
+
+		List<GameButton> doInit() {
+			var gameButtons = <GameButton>[
+				new GameButton(id: 1),
+				new GameButton(id: 2),
+				new GameButton(id: 3),
+				new GameButton(id: 4),
+				new GameButton(id: 5),
+				new GameButton(id: 6),
+				new GameButton(id: 7),
+				new GameButton(id: 8),
+				new GameButton(id: 9),
+			];
+			return gameButtons;
+		}
 	@override
 	Widget build(BuildContext context) {
-		return new Container();
+		return new Scaffold(
+			appBar: new AppBar(
+				title: new Text("Tic Tak Toe"),
+			),
+			body: new GridView.builder(
+				padding: const EdgeInsets.all(10.0),
+				gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+					crossAxisCount: 3,
+					childAspectRatio: 1.0,
+					crossAxisSpacing: 9.0,
+					mainAxisSpacing: 9.0,
+				),
+				itemCount: buttonsList.length,
+				itemBuilder: (context, i) => new SizedBox(
+					width: 100.0,
+					height: 100.0,
+					child: new RaisedButton(
+						padding:  const EdgeInsets.all(8.0),
+						onPressed: null,
+						child: new Text(
+							buttonsList[i].text,
+							style: new TextStyle(color: Colors.white, fontSize: 20.0),
+						),
+						color: buttonsList[i].bg,
+						disabledColor: buttonsList[i].bg,
+					),
+				),
+			));
 	}
 }
